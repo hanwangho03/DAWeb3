cb8d39a160d275b96537862b0d7217c9fde43a51
The Bao <anhthuan06042003@gmail.com>
2024-04-17 14:15:48 +0700

    85%

---
+++ DAWeb3/Controllers/Admin2Controller.cs
@@ -0,0 +1,142 @@
+﻿using DAWeb3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAWeb3.Controllers
+{
+    public class Admin2Controller : Controller
+    {
+        private readonly WebTracNghiemContext _context;
+
+        public Admin2Controller(WebTracNghiemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin2
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Admins.ToListAsync());
+        }
+
+        // GET: Admin2/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin2/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TaiKhoan,MatKhau,DaXoa")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(admin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admin);
+        }
+
+        // GET: Admin2/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admin2/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("TaiKhoan,MatKhau,DaXoa")] Admin admin)
+        {
+            if (id != admin.TaiKhoan)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(admin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdminExists(admin.TaiKhoan))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admin);
+        }
+
+        // GET: Admin2/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admins
+                .FirstOrDefaultAsync(m => m.TaiKhoan == id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return View(admin);
+        }
+
+        // POST: Admin2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+            _context.Admins.Remove(admin);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admins
+                .FirstOrDefaultAsync(m => m.TaiKhoan == id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return View(admin);
+        }
+        private bool AdminExists(string id)
+        {
+            return _context.Admins.Any(e => e.TaiKhoan == id);
+        }
+    }
+}
